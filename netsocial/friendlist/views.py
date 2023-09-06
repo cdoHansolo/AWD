d677976d0f9f89681a00ed045a72a13feae493ce
Hangitsolo <alvin.han0406@gmail.com>
2023-09-06 18:20:35 +0800

    login/register/profile

--- netsocial/friendlist/views.py
+++ netsocial/friendlist/views.py
@@ -1,14 +1,39 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
+from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-# Create your views here.
+# Create your views here
+
+def home_view(request):
+    return render(request, 'friendlist/homepage.html')
+
 def register_view(request):
     if request.method =="POST":
         form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
-            return redirect('registration_success')
+            return redirect('profile')
     else:
         form = RegisterForm()
     
     return render(request, 'friendlist/register_form.html', {'form': form})
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('profile') #Redirect to user's profile
+        else:
+            messages.error(request, 'Invalid username or password')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'friendlist/loginpage.html', {'form': form})
+
+def profile_view(request):
+    user = request.user #user successfully login
+    context = {'user':user}
+    return render(request, 'friendlist/profilepage.html', context)
--- netsocial/netsocial/urls.py
+++ netsocial/netsocial/urls.py
@@ -3,7 +3,10 @@ from django.urls import path
 from friendlist import views
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
-    path('register/', views.register_view, name='registration_view'),
-    path('registration_success/', views.registration_success, name='registration_success'),
+    path('', views.home_view, name='home'),
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
+    path('profile/', views.profile_view, name='profile'),
 ]
