6a210be3e9a09dfad790963771fa622bedf54275
Hangitsolo <alvin.han0406@gmail.com>
2023-09-11 17:16:05 +0800

    btn

--- netsocial/friendlist/admin.py
+++ netsocial/friendlist/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import FriendRequest
 from .models import UserProfile
+from .models import Friend
 
 # Register your models here.
 admin.site.register(FriendRequest)
 admin.site.register(UserProfile)
+admin.site.register(Friend)
